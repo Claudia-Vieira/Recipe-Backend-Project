d2ec1480079c59cadb6dbcc8538552d46652bb14
Claudia Vieira <clau_f.v@hotmail.com>
2022-01-16 19:25:52 +0000

    Add project files.

---
+++ Models/RecipeIngredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Backend.Models
+{
+    public class RecipeIngredient
+    {
+        public Recipe Recipes { get; set; }
+        public int RecipeId { get; set; }
+        public Ingredient Ingredients { get; set; }
+        public int IngredientId { get; set; }
+    }
+}
